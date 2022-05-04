2db1cedf9cf59cf0bab693a4de1e2729599f508b
John Marcos <johnsaguay@gmail.com>
2022-05-04 15:07:01 -0700

    adding tab bar controller

---
+++ Bot-Shop/PastOrderViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PastOrderViewController.swift
+//  Bot-Shop
+//
+//  Created by John Sag on 5/4/22.
+//
+
+import Foundation
+import UIKit
+
+class PastOrderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .green
+    }
+
+}
